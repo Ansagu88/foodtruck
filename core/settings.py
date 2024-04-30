2ab752f80971b19f0569beb6de79740318929336
ansagu88 <ansagu88@gmail.com>
2024-04-30 20:49:06 -0300

    Change email settngs

--- core/settings.py
+++ core/settings.py
@@ -202,8 +202,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'andecoreing@gmail.com'  # Tu correo
-EMAIL_HOST_PASSWORD = 'mwhd ceth lmii bubc'  # Tu contraseña
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")  # Tu correo
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  # Tu contraseña
 
 
 # Google Api Key
